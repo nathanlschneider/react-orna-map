534d27ea7fb0c220feec1696f8d0783d10c870ea
Nathan Schneider <nlschneider@gmail.com>
2019-07-06 01:41:09 -0400

    dynamic icon zoom sizing and drift fix

--- src/Map.js
+++ src/Map.js
@@ -33,10 +33,33 @@ export default class Map extends Component {
     this._handleSlideMenu = this._handleSlideMenu.bind(this);
   }
 
-  _handleSlideMenu = () => this.setState({menuIsOpen: !this.state.menuIsOpen});
-  _handleChange = (e) => this.setState({ canEdit: e.target.checked, menuIsOpen: !this.state.menuIsOpen});
+  _handleSlideMenu = () =>
+    this.setState({ menuIsOpen: !this.state.menuIsOpen });
+  _handleChange = e =>
+    this.setState({
+      canEdit: e.target.checked,
+      menuIsOpen: !this.state.menuIsOpen
+    });
+
+  _onViewportChange(viewport) {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  }
+  _resize = () => {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this._resize);
+  }
 
   componentDidMount() {
+    window.addEventListener("resize", this._resize);
+    this._resize();
     this.setState({
       latitude: this.props.latitude,
       longitude: this.props.longitude,
@@ -58,6 +81,11 @@ export default class Map extends Component {
   }
 
   render() {
+    const size = 10;
+    const { zoom } = this.state.viewport;
+
+    console.log(zoom);
+
     if (this.props.markerData !== undefined && this.props.markerData !== null) {
       return (
         <ReactMapGL
@@ -86,6 +114,7 @@ export default class Map extends Component {
                 latitude={list[index][1][1]}
               >
                 <img
+                  className="mapIcon"
                   src={
                     icon === "Shop"
                       ? Shop
@@ -107,15 +136,36 @@ export default class Map extends Component {
                       ? Keep
                       : Outpost
                   }
-                  width={this.state.viewport["zoom"] * 5}
+                  width={
+                    zoom <= 15
+                      ? zoom * 2
+                      : zoom <= 15.5
+                      ? zoom * 3
+                      : zoom <= 16
+                      ? zoom * 4
+                      : zoom <= 16.5
+                      ? zoom * 5
+                      : zoom <= 17
+                      ? zoom * 6
+                      : zoom <= 17.5
+                      ? zoom * 7
+                      : zoom <= 18
+                      ? zoom * 8
+                      : zoom <= 18.5
+                      ? zoom * 9
+                      : zoom <= 19
+                      ? zoom * 10
+                      : zoom * 11
+                  }
                   style={
                     this.state.canEdit
                       ? {
                           opacity: 0.5,
                           backgroundColor: "red",
-                          borderRadius: "5px"
+                          borderRadius: "5px",
+                          transform: `translate(${-size / 1.8}px,${-size}px)`
                         }
-                      : {}
+                      : { transform: `translate(${-size / 1.8}px,${-size}px)` }
                   }
                   alt=""
                 />
@@ -124,16 +174,32 @@ export default class Map extends Component {
           })}
 
           <div
-            
             className="slideMenu nes-container is-rounded"
-            style={this.state.menuIsOpen ? {transform: 'translateX(0)'}:{transform: 'translateX(-75vw)'}}
+            style={
+              this.state.menuIsOpen
+                ? { transform: "translateX(0)" }
+                : { transform: "translateX(-75vw)" }
+            }
           >
             <label>
-              <input type="checkbox" className="slideMenu-checkbox nes-checkbox" onChange={this._handleChange}/>
+              <input
+                type="checkbox"
+                className="slideMenu-checkbox nes-checkbox"
+                onChange={this._handleChange}
+              />
               <span>Edit Mode</span>
             </label>
-            <div className="slideMenu-btn" onClick={this._handleSlideMenu} style={this.state.menuIsOpen ? {transform: 'rotate(180deg)' }:{transform: 'rotate(0deg)'}}>></div>
-           
+            <div
+              className="slideMenu-btn"
+              onClick={this._handleSlideMenu}
+              style={
+                this.state.menuIsOpen
+                  ? { transform: "rotate(180deg)" }
+                  : { transform: "rotate(0deg)" }
+              }
+            >
+              >
+            </div>
           </div>
         </ReactMapGL>
       );
